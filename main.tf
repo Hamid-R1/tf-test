ae1e9b85e65838b396e70776362875bdc1b5f117
Hamid-R1 <112654341+Hamid-R1@users.noreply.github.com>
2024-02-19 00:22:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,48 @@
+# terraform block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.3.0"
+    }
+  }
+}
+
+# provider block
+provider "aws" {
+  region = "ap-southeast-1" #Asia Pacific (Singapore)#
+}
+
+
+# Data source:  Get AMI ID for Amazon Linux2 OS
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*-gp2"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+
+# bastion-server in public-subnet-1
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+  key_name      = "hammy-singapore-key"
+  tags = {
+    Name = "ec2-tf"
+  }
+}
